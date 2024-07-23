59d3edbd909cbb923f18185710a6c49733692279
Cloé Cordeiro <73696923+CloeCrdr@users.noreply.github.com>
2024-07-23 15:51:25 +0200

    update ci-cd

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# Aller dans le répertoire de l'application
 cd /home/ec2-user/fastapi_template
 
-# Pull les dernières modifications du dépôt
 git pull origin main
 
-# Installer les dépendances
 pip install -r requirements.txt
 
-# Redémarrer le service (ajustez selon votre configuration)
 sudo systemctl restart nginx
